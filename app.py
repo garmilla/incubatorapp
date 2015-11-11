6bd9cfcc8665e9e8acd4890ac1fdb201ea116d04
Jose Garmilla <garmilla@princeton.edu>
2015-11-11 21:21:47 +0900

    Change api and add a 5th series

--- app.py
+++ app.py
@@ -54,14 +54,18 @@ def fetchSeriesQuandl(database, dataset, columnName):
     series.index = pd.to_datetime(series.index)
     return series
 
-def combineSeries(series1, series2):
+def combineSeries(series1, series2, overrideStart=None, overrideEnd=None):
     """
     Combine two series in such a way that they can be plotted together.
     For series that have higher time resolution we apply a moving
     average.
     """
-    start = max(series1.index[0], series2.index[1])
+    start = max(series1.index[0], series2.index[0])
+    if overrideStart is not None:
+        start = overrideStart
     end = min(series1.index[-1], series2.index[-1])
+    if overrideEnd is not None:
+        end = overrideEnd
     delta1 = series1.index[1] - series1.index[0]
     delta2 = series2.index[1] - series2.index[0]
     if delta1 < delta2:
@@ -99,34 +103,62 @@ def combineSeries(series1, series2):
         mask = maskStart & maskEnd
         timestamps2 = series2[mask].index
         values2 = series2[mask].values
-        assert timestamps1 == timestamps2
+        #assert timestamps1 == timestamps2
         timestamps = timestamps1
     return timestamps, values1, values2
 
-def plotSeries(series1, series2, title, label1, label2, start2, end2):
-    timestamps, values1, values2 = combineSeries(series1, series2)
-    chart = figure(x_axis_type="datetime", x_axis_label="Time", y_axis_label=label1, tools=TOOLS)
+def plotSeries(seriesTuple, title, labels, colors):
+    deltaMax = seriesTuple[0].index[1] - seriesTuple[0].index[0]
+    start = seriesTuple[0].index[0]
+    end = seriesTuple[0].index[-1]
+    valMax = seriesTuple[0].values.max()
+    idxValMax = 0
+    idxDeltaMax = 0
+    for i in range(len(seriesTuple)):
+        series = seriesTuple[i]
+        delta = series.index[1] - series.index[0]
+        if delta > deltaMax:
+            deltaMax = delta
+            idxDeltaMax = i
+        if series.values.max() > valMax: 
+            valMax = series.values.max()
+            idxValMax = i
+        if series.index[0] > start:
+            start = series.index[0]
+        if series.index[-1] < end:
+            end = series.index[-1]
+    valuesList = []
+    for i in range(len(seriesTuple)):
+        timestamps, valuesDeltaMax, values = combineSeries(seriesTuple[idxDeltaMax], seriesTuple[i], overrideStart=start, overrideEnd=end)
+        valuesList.append(values)
+    chart = figure(x_axis_type="datetime", x_axis_label="Time", y_axis_label=labels[idxValMax], tools=TOOLS)
     chart.title = title
-    chart.extra_y_ranges = {"Y2": Range1d(start=start2, end=end2)}
-    chart.add_layout(LinearAxis(y_range_name="Y2", axis_label=label2), 'right')
-    chart.line(timestamps, values1, legend=label1, color='blue')
-    chart.line(timestamps, values2, legend=label2, y_range_name="Y2", color='red')
+    for i in range(len(seriesTuple)):
+        if i != idxValMax:
+            series = seriesTuple[i]
+            chart.extra_y_ranges["Y{0}".format(i)] = Range1d(start=series.values.min(), end=series.values.max())
+            chart.add_layout(LinearAxis(y_range_name="Y{0}".format(i), axis_label=labels[i]), 'right')
+    chart.line(timestamps, valuesList[idxValMax], legend=labels[idxValMax], color=colors[idxValMax])
+    for i in range(len(seriesTuple)):
+        if i != idxValMax:
+            chart.line(timestamps, valuesList[i], legend=labels[i], y_range_name="Y{0}".format(i), color=colors[i])
     return chart
 
 output_file('templates/index.html')
 seriesFred = fetchSeriesFred('ISRATIO')
 seriesQuandl = fetchSeriesQuandl('ODA', 'POILAPSP_INDEX', 'Value')
-chart1 = plotSeries(seriesQuandl, seriesFred, "Inventory to Sales Ratio vs Blended Crude Oil", "OIL", "ISRATIO", 1.2, 1.55)
+chart1 = plotSeries((seriesQuandl, seriesFred), "Inventory to Sales Ratio vs Blended Crude Oil", ("OIL", "ISRATIO"), ("blue", "red"))
 seriesFred = fetchSeriesFred('CIVPART')
-chart2 = plotSeries(seriesQuandl, seriesFred, "Labor Force Participation vs Blended Crude Oil", "OIL", "LFPART", 58.0, 68.0)
+chart2 = plotSeries((seriesQuandl, seriesFred), "Labor Force Participation vs Blended Crude Oil", ("OIL", "LFPART"), ("blue", "red"))
 seriesQuandl2 = fetchSeriesQuandl('YAHOO', 'INDEX_GSPC', 'Close')
 seriesFred = fetchSeriesFred('WALCL')
-chart3 = plotSeries(seriesFred, seriesQuandl2, "Fed Balance Sheet vs S&P 500", "WALCL", "S&P 500", 500.0, 2050.0)
+chart3 = plotSeries((seriesFred, seriesQuandl2), "Fed Balance Sheet vs S&P 500", ("WALCL", "S&P 500"), ("blue", "red"))
 chart3.legend.orientation = 'bottom_right'
 seriesQuandl2 = fetchSeriesQuandl('GOOG', 'NYSE_SEA', 'Close')
-#seriesQuandl3 = fetchSeriesQuandl('LLOYDS', 'BDI', 'Index')
-chart4 = plotSeries(seriesQuandl, seriesQuandl2, "Global Shipping Index SEA vs Blended Crude Oil", "OIL", "SEA", 8.0, 30.0)
-save(vplot(chart1, chart2, chart3, chart4))
+seriesQuandl3 = fetchSeriesQuandl('LLOYDS', 'BDI', 'Index')
+chart4 = plotSeries((seriesQuandl, seriesQuandl2), "Global Shipping Index SEA vs Blended Crude Oil", ("OIL", "SEA"), ("blue", "red"))
+chart5 = plotSeries((seriesQuandl2, seriesQuandl3), "Global Shipping Index SEA vs Baltic Dry Index", ("SEA", "BDI"), ("blue", "red"))
+save(vplot(chart1, chart2, chart3, chart4, chart5))
 
 @app.route('/')
 def main():
